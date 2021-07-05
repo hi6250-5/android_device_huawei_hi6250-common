de7871184cfd69ae73002f9de6c29cbb5184690b
A2L5E0X1 <alexed25@protonmail.com>
2021-07-05 20:44:24 +0200

    hi6250: import init files and ramdisk firmware
    Change-Id: I00d8f839889e2bc51748f9aa33d6480f0405bd0e

--- hi6250.mk
+++ hi6250.mk
@@ -23,7 +23,39 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Init
 PRODUCT_PACKAGES += \
-    init.recovery.hi6250.rc
+    fstab.hi6250 \
+    init.audio.rc \
+    init.balong_modem.rc \
+    init.charger.rc \
+    init.chip.charger.rc \
+    init.connectivity.bcm43455.rc \
+    init.connectivity.gps.rc \
+    init.connectivity.hisi.rc \
+    init.connectivity.rc \
+    init.device.rc \
+    init.extmodem.rc \
+    init.hi6250.rc \
+    init.hisi.rc \
+    init.hisi.usb.rc \
+    init.manufacture.rc \
+    init.platform.rc \
+    init.post-fs-data.rc \
+    init.protocol.rc \
+    init.recovery.hi6250.rc \
+    init.tee.rc \
+    ueventd.hi6250.rc
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    bfgx_and_wifi_cfg \
+    bfgx_cfg \
+    CPU_RAM_SCHED.bin \
+    CPU_RAM_WBS.bin \
+    cr4_asic.bin \
+    cr4_regmem.bin \
+    cr4_regmem_u1.bin \
+    ram_reg_test_cfg \
+    wifi_cfg
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(COMMON_PATH)
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,8 +17,212 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := bfgx_and_wifi_cfg
+LOCAL_SRC_FILES := firmware/bfgx_and_wifi_cfg
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := bfgx_cfg
+LOCAL_SRC_FILES := firmware/bfgx_cfg
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := CPU_RAM_SCHED.bin
+LOCAL_SRC_FILES := firmware/CPU_RAM_SCHED.bin
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := CPU_RAM_WBS.bin
+LOCAL_SRC_FILES := firmware/CPU_RAM_WBS.bin
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := cr4_asic.bin
+LOCAL_SRC_FILES := firmware/cr4_asic.bin
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := cr4_regmem.bin
+LOCAL_SRC_FILES := firmware/cr4_regmem.bin
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := cr4_regmem_u1.bin
+LOCAL_SRC_FILES := firmware/cr4_regmem_u1.bin
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fstab.hi6250
+LOCAL_SRC_FILES := etc/fstab.hi6250
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := init.recovery.hi6250.rc
 LOCAL_SRC_FILES := etc/init.recovery.hi6250.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_OUT)/root
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.audio.rc
+LOCAL_SRC_FILES := etc/init.audio.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.balong_modem.rc
+LOCAL_SRC_FILES := etc/init.balong_modem.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.charger.rc
+LOCAL_SRC_FILES := etc/init.charger.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.chip.charger.rc
+LOCAL_SRC_FILES := etc/init.chip.charger.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.connectivity.bcm43455.rc
+LOCAL_SRC_FILES := etc/init.connectivity.bcm43455.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.connectivity.gps.rc
+LOCAL_SRC_FILES := etc/init.connectivity.gps.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.connectivity.hisi.rc
+LOCAL_SRC_FILES := etc/init.connectivity.hisi.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.connectivity.rc
+LOCAL_SRC_FILES := etc/init.connectivity.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.device.rc
+LOCAL_SRC_FILES := etc/init.device.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.extmodem.rc
+LOCAL_SRC_FILES := etc/init.extmodem.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.hi6250.rc
+LOCAL_SRC_FILES := etc/init.hi6250.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.hisi.rc
+LOCAL_SRC_FILES := etc/init.hisi.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.hisi.usb.rc
+LOCAL_SRC_FILES := etc/init.hisi.usb.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.manufacture.rc
+LOCAL_SRC_FILES := etc/init.manufacture.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.platform.rc
+LOCAL_SRC_FILES := etc/init.platform.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.post-fs-data.rc
+LOCAL_SRC_FILES := etc/init.post-fs-data.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.protocol.rc
+LOCAL_SRC_FILES := etc/init.protocol.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.tee.rc
+LOCAL_SRC_FILES := etc/init.tee.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ram_reg_test_cfg
+LOCAL_SRC_FILES := firmware/ram_reg_test_cfg
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ueventd.hi6250.rc
+LOCAL_SRC_FILES := etc/ueventd.hi6250.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := wifi_cfg
+LOCAL_SRC_FILES := firmware/wifi_cfg
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
